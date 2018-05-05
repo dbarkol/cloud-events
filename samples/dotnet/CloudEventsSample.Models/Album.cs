c297a5bede8dee6f668680d073818dc0f59741ba
David Barkol <dbarkol@hotmail.com>
2018-05-05 09:34:05 -0700

    Initial revision

---
+++ samples/dotnet/CloudEventsSample.Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudEventsSample.Models
+{
+    public class Album
+    {
+        public string AlbumName { get; set; }
+
+        public int YearReleased { get; set; }
+    }
+}
---
+++ samples/dotnet/CloudEventsSample.Models/Band.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudEventsSample.Models
+{
+    public class Band
+    {
+        public string Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
